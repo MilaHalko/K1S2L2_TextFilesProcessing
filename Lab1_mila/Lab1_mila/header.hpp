3b7ef7b6125fcb81c9798d44831dfe164e7235a5
milka_deluxe <milagalko1@gmail.com>
2021-02-25 16:42:52 +0200

    FCs' array
    array [20] - names of football clubs

--- Lab1_mila/Lab1_mila/header.hpp
+++ Lab1_mila/Lab1_mila/header.hpp
@@ -3,5 +3,10 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
 
 using namespace std;
+
+int F_numb_of_clubs(string);
+void F_array_of_clubs(string*, string, string, int, int);
+void F_print_array(string*, int);
